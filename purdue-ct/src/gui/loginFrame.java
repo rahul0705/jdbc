9f074e25d87252327df5b6397800ea5592ea4211
rahul0705 <rahul0705@gmail.com>
2013-04-09 01:12:50 -0400

    Combined the Panel and the Frams together

--- purdue-ct/src/gui/loginFrame.java
+++ purdue-ct/src/gui/loginFrame.java
@@ -1,15 +1,36 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Enumeration;
+
+@SuppressWarnings("serial")
 public class loginFrame extends JFrame {
-
 	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JPasswordField pwdPassword;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -32,9 +53,111 @@ public class loginFrame extends JFrame {
 	 */
 	public loginFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 360, 200);
-		contentPane = new loginPanel();
+		setBounds(100, 100, 360, 230);
+		contentPane = new JPanel();
 		setContentPane(contentPane);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{30, 150, 150, 30};
+		gridBagLayout.rowHeights = new int[]{43, 43, 43, 43};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		setLayout(gridBagLayout);
+
+		JLabel lblUserId = new JLabel("User ID:");
+		lblUserId.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblUserId = new GridBagConstraints();
+		gbc_lblUserId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblUserId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUserId.gridx = 1;
+		gbc_lblUserId.gridy = 0;
+		add(lblUserId, gbc_lblUserId);
+
+		txtUsername = new JTextField();
+		txtUsername.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_txtUsername = new GridBagConstraints();
+		gbc_txtUsername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_txtUsername.gridx = 2;
+		gbc_txtUsername.gridy = 0;
+		add(txtUsername, gbc_txtUsername);
+		txtUsername.setColumns(10);
+
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPassword.gridx = 1;
+		gbc_lblPassword.gridy = 1;
+		add(lblPassword, gbc_lblPassword);
+
+		pwdPassword = new JPasswordField();
+		pwdPassword.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_pwdPassword = new GridBagConstraints();
+		gbc_pwdPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_pwdPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_pwdPassword.gridx = 2;
+		gbc_pwdPassword.gridy = 1;
+		add(pwdPassword, gbc_pwdPassword);
+
+		JRadioButton rdbtnFaculty = new JRadioButton("Faculty");
+		rdbtnFaculty.setHorizontalAlignment(SwingConstants.CENTER);
+		buttonGroup.add(rdbtnFaculty);
+		GridBagConstraints gbc_rdbtnFaculty = new GridBagConstraints();
+		gbc_rdbtnFaculty.fill = GridBagConstraints.BOTH;
+		gbc_rdbtnFaculty.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnFaculty.gridx = 1;
+		gbc_rdbtnFaculty.gridy = 2;
+		add(rdbtnFaculty, gbc_rdbtnFaculty);
+
+		JRadioButton rdbtnStudent = new JRadioButton("Student");
+		rdbtnStudent.setHorizontalAlignment(SwingConstants.CENTER);
+		buttonGroup.add(rdbtnStudent);
+		GridBagConstraints gbc_rdbtnStudent = new GridBagConstraints();
+		gbc_rdbtnStudent.fill = GridBagConstraints.BOTH;
+		gbc_rdbtnStudent.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnStudent.gridx = 2;
+		gbc_rdbtnStudent.gridy = 2;
+		add(rdbtnStudent, gbc_rdbtnStudent);
+
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = txtUsername.getText();
+				char[] password = pwdPassword.getPassword();
+				System.out.println(username);
+				System.out.println(password);
+				for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+					AbstractButton button = buttons.nextElement();
+					if (button.isSelected()) {
+						System.out.println(button.getText());
+					}
+				}
+				dispose();
+				toolsFrame frame = new toolsFrame();
+				frame.setVisible(true);
+			}
+		});
+		GridBagConstraints gbc_btnOk = new GridBagConstraints();
+		gbc_btnOk.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnOk.insets = new Insets(0, 0, 0, 5);
+		gbc_btnOk.gridx = 1;
+		gbc_btnOk.gridy = 3;
+		add(btnOk, gbc_btnOk);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+		gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnCancel.gridx = 2;
+		gbc_btnCancel.gridy = 3;
+		add(btnCancel, gbc_btnCancel);
+		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{txtUsername, pwdPassword, rdbtnFaculty, rdbtnStudent, btnOk, btnCancel}));
 	}
 
 }
---
+++ purdue-ct/src/gui/toolsFrame.java
@@ -0,0 +1,87 @@
+package gui;
+
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class toolsFrame extends JFrame {
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					toolsFrame frame = new toolsFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public toolsFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 200);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblWelcome = new JLabel("Welcome");
+		GridBagConstraints gbc_lblWelcome = new GridBagConstraints();
+		gbc_lblWelcome.insets = new Insets(0, 0, 5, 0);
+		gbc_lblWelcome.gridx = 0;
+		gbc_lblWelcome.gridy = 0;
+		add(lblWelcome, gbc_lblWelcome);
+		
+		JButton btnMyMessages = new JButton("My Messages");
+		GridBagConstraints gbc_btnMyMessages = new GridBagConstraints();
+		gbc_btnMyMessages.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnMyMessages.insets = new Insets(0, 0, 5, 0);
+		gbc_btnMyMessages.gridx = 0;
+		gbc_btnMyMessages.gridy = 1;
+		add(btnMyMessages, gbc_btnMyMessages);
+		
+		JButton btnCalendarOfEvaluations = new JButton("Calendar of Evaluations");
+		GridBagConstraints gbc_btnCalendarOfEvaluations = new GridBagConstraints();
+		gbc_btnCalendarOfEvaluations.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnCalendarOfEvaluations.insets = new Insets(0, 0, 5, 0);
+		gbc_btnCalendarOfEvaluations.gridx = 0;
+		gbc_btnCalendarOfEvaluations.gridy = 2;
+		add(btnCalendarOfEvaluations, gbc_btnCalendarOfEvaluations);
+		
+		JButton btnMyClasses = new JButton("My Classes");
+		GridBagConstraints gbc_btnMyClasses = new GridBagConstraints();
+		gbc_btnMyClasses.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnMyClasses.insets = new Insets(0, 0, 5, 0);
+		gbc_btnMyClasses.gridx = 0;
+		gbc_btnMyClasses.gridy = 3;
+		add(btnMyClasses, gbc_btnMyClasses);
+		
+		JButton btnMyGrades = new JButton("My Grades");
+		GridBagConstraints gbc_btnMyGrades = new GridBagConstraints();
+		gbc_btnMyGrades.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnMyGrades.gridx = 0;
+		gbc_btnMyGrades.gridy = 4;
+		add(btnMyGrades, gbc_btnMyGrades);
+	}
+
+}
